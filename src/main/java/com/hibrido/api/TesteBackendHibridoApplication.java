ff2980df8d6ecce5edbd925c571147fefd694c7f
Lucas Pereira <lucas.plima.ti@gmail.com>
2021-02-03 23:04:06 -0300

    Configuração inicial do projeto.

---
+++ src/main/java/com/hibrido/api/TesteBackendHibridoApplication.java
@@ -0,0 +1,13 @@
+package com.hibrido.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteBackendHibridoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteBackendHibridoApplication.class, args);
+	}
+
+}
